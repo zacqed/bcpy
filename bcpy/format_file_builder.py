f2ea9e3d5a5398644d7835fe044624f0db2a2d8e
zacqed <harshithg@gmail.com>
2024-12-24 17:42:10 +0530

    Update format_file_builder.py

--- bcpy/format_file_builder.py
+++ bcpy/format_file_builder.py
@@ -5,19 +5,14 @@ class FormatFile:
         :param flat_file: Source flat file
         :type flat_file: FlatFile
         """
-        terminators = list()
+        terminators = []
         qualifier = cls._scaper(flat_file.qualifier)
         delimiter = cls._scaper(flat_file.delimiter)
         newline = cls._scaper(flat_file.newline)
         if flat_file.qualifier:
             terminators.append(qualifier)
-        qualifier_delimiter_combo = str.format(
-            '{0}{1}{0}',
-            qualifier,
-            delimiter)
-        terminators.extend(
-            [qualifier_delimiter_combo
-             for _ in range(len(flat_file.columns) - 1)])
+        qualifier_delimiter_combo = str.format("{0}{1}{0}", qualifier, delimiter)
+        terminators.extend([qualifier_delimiter_combo for _ in range(len(flat_file.columns) - 1)])
         terminators.append(qualifier + newline)
         return terminators
 
@@ -28,8 +23,7 @@ class FormatFile:
         :type input_string: str
         :return: Scaped string
         """
-        scaped_string = input_string.replace('"', '\\"').replace("'", "\\'") \
-            .replace('\r', '\\r').replace('\n', '\\n')
+        scaped_string = input_string.replace('"', '\\"').replace("'", "\\'").replace("\r", "\\r").replace("\n", "\\n")
         return scaped_string
 
     @classmethod
@@ -44,19 +38,22 @@ class FormatFile:
             format_file_row_count = len(data_object.columns) + 1
         else:
             format_file_row_count = len(data_object.columns)
-        format_file = f'9.0\n{format_file_row_count}\n'
+        format_file = f"9.0\n{format_file_row_count}\n"
         terminators = cls._get_field_terminators(data_object)
         if data_object.qualifier:
-            format_file += f'1 SQLCHAR 0 0 "{terminators[0]}" 0 ' \
-                f'ignored_line_start_qualifier SQL_Latin1_General_CP1_CI_AS\n'
+            format_file += (
+                f'1 SQLCHAR 0 0 "{terminators[0]}" 0 ' f"ignored_line_start_qualifier SQL_Latin1_General_CP1_CI_AS\n"
+            )
             format_file_row_index = 2
             terminators = terminators[1:]
         else:
             format_file_row_index = 1
         for column_index, terminator in enumerate(terminators, 1):
-            format_file += f'{format_file_row_index} SQLCHAR 0 0 ' \
-                f'"{terminator}" {column_index} ' \
-                f'{data_object.columns[column_index - 1]} ' \
-                f'SQL_Latin1_General_CP1_CI_AS\n'
+            format_file += (
+                f"{format_file_row_index} SQLCHAR 0 0 "
+                f'"{terminator}" {column_index} '
+                f"{data_object.columns[column_index - 1]} "
+                f"SQL_Latin1_General_CP1_CI_AS\n"
+            )
             format_file_row_index += 1
         return format_file
